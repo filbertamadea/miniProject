f9626100e1a29c7c90fdb6861ccccff32879b747
aloysius.editiyan <aloysius.editiyan@gmail.com>
2023-11-28 00:41:28 +0700

    Add Fetch to mockapi

--- miniproject/src/components/Menu.vue
+++ miniproject/src/components/Menu.vue
@@ -5,12 +5,33 @@
         <v-col>
           <v-card>
             <v-card-title>{{ recipeName }}</v-card-title>
+
+            <v-card-text :style="{ marginTop: '16px', marginBottom: '-16px' }">
+              INGREDIENTS
+            </v-card-text>
+
+            <v-card-text>
+              <v-expansion-panels>
+                <v-expansion-panel
+                  v-for="itemg in ingredients"
+                  :key="itemg.createdAt"
+                  :title="itemg.name"
+                  :text="itemg.description"
+                >
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-card-text>
+
+            <v-card-text :style="{ marginTop: '16px', marginBottom: '-16px' }">
+              INSTRUCTIONS
+            </v-card-text>
+
             <v-card-text>
               <v-expansion-panels>
                 <v-expansion-panel
                   v-for="item in recipeSteps"
-                  :key="item.title"
-                  :title="item.title"
+                  :key="item.createdAt"
+                  :title="item.todo"
                   :text="item.description"
                 >
                 </v-expansion-panel>
@@ -26,19 +47,36 @@
 <script setup>
 import { ref, onMounted } from "vue";
 let recipeSteps = ref([]);
-let recipeName = ref('');
+let ingredients = ref([]);
+let recipeName = ref("");
 
-const init = () => {
-  recipeName.value = "CUKURUKUK"
-  recipeSteps.value = [
-    { title: "Step 1", description: "Description for Step 1 goes here." },
-    { title: "Step 2", description: "Description for Step 2 goes here." },
-    { title: "Step 3", description: "Description for Step 3 goes here." },
-    // Add more steps as needed
-  ];
+// Define a function to fetch data
+const fetchData = async () => {
+  try {
+    const responseIngredients = await fetch( "https://6564bb89ceac41c0761eba72.mockapi.io/api/miniproject/ingredients" );
+    const responseInstructions = await fetch( "https://6564bb89ceac41c0761eba72.mockapi.io/api/miniproject/instruction" );
+
+    if (responseIngredients.ok) {
+      const responseIngredientsTemp = await responseIngredients.json();
+      recipeName.value = "Resep Nasi Goreng Spesial";
+      ingredients.value = responseIngredientsTemp;
+
+      if (responseInstructions.ok) {
+        const responseInstructionsTemp = await responseInstructions.json();
+        recipeSteps.value = responseInstructionsTemp;
+      } else {
+        console.error("Failed to fetch data");
+      }
+
+    } else {
+      console.error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 };
 
-onMounted(init);
+onMounted(fetchData);
 </script>
 
 <style>
